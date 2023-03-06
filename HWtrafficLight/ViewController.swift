08e210f11affc0ff4e5f5b356ae438650ca3c588
OleksiiHolub <holub.oleksii@outlook.com>
2023-03-06 23:47:05 +0200

    finish with traffic light

--- HWtrafficLight/ViewController.swift
+++ HWtrafficLight/ViewController.swift
@@ -15,10 +15,40 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        redLightView.alpha = 0.3
+        yellowLightView.alpha = 0.3
+        greenLightView.alpha = 0.3
+        
+        redLightView.layer.cornerRadius = 50
+        yellowLightView.layer.cornerRadius = 50
+        greenLightView.layer.cornerRadius = 50
+        
+        buttonOfStart.layer.cornerRadius = 20
     }
 
     @IBAction func buttonOfStartPressed() {
+        
+        if redLightView.alpha != 1 && yellowLightView.alpha != 1 {
+            
+            redLightView.alpha = 1
+            yellowLightView.alpha = 0.3
+            greenLightView.alpha = 0.3
+            buttonOfStart.setTitle("Next", for: .normal)
+            print("Red")
+            
+        } else if yellowLightView.alpha != 1 {
+            yellowLightView.alpha = 1
+            redLightView.alpha = 0.3
+            greenLightView.alpha = 0.3
+            print("yellow")
+            
+        } else if greenLightView.alpha != 1 {
+            greenLightView.alpha = 1
+            redLightView.alpha = 0.3
+            yellowLightView.alpha = 0.3
+            print("green")
+        }
     }
     
 }
